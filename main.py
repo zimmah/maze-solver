566eeb9728c762e3b39591dbc361b21a4ec1e547
zimmah <zimmah@gmail.com>
2024-08-16 21:10:20 -0500

    add cell/opt/homebrew/bin/python3

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 from graphics import Window
-from cell import Cell
+from maze import Maze
 
 def main():
-    win = Window(800, 600)
-    cell1 = Cell(50, 100, 50, 100, win)
-    cell1.has_right_wall = False
-    cell2 = Cell(100, 150, 50, 100, win)
-    cell2.has_left_wall = False
-    cell2.has_bottom_wall = False
-    cell3 = Cell(100, 150, 100, 150, win)
-    cell3.has_top_wall = False
-    cell3.has_right_wall = False
-    cell4 = Cell(150, 200, 100, 150, win)
-    cell4.has_left_wall = False
-    cell1.draw()
-    cell2.draw()
-    cell3.draw()
-    cell4.draw()
-    cell3.draw_move(cell4, True)
-    win.wait_for_close()
+    window = Window(800, 600)
+    maze = Maze(0, 0, 10, 10, 50, 50, window)
+    for i in len(maze._cells):
+        for j in len(maze[i]):
+            maze._draw_cell[i][j]
+    window.wait_for_close()
+    
     
 main()
